3ff0ffc5e1268defb6d938ff93853ee4c78c7053
sumi005 <salli7625@richmondhillhs.org>
2022-05-23 02:36:28 +0000

    demo--nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import './App.css';
+import {About} from './Components/about';
+import {Contact} from './Components/contact';
+import {Home} from './Components/home';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  render() {
+function App(){
     return (
-      <div className="App">
-        <h1>Hello React App</h1>
+        <BrowserRouter>
+        <div className="App">
+        <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link as={Link} to ="/">Home</Nav.Link>
+      <Nav.Link as={Link} to ="/About"> About</Nav.Link>
+      <Nav.Link as={Link} to ="/Contact"> Contact</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  </>
+    <div>
+      <Routes>
+          <Route path="/about" element= {<About/>}/>
+          <Route path="/contact" element= {<Contact/>}/>
+          <Route path="/" element= {<Home/>}/>
+      </Routes>
       </div>
+      </div>
+      </BrowserRouter>
     );
-  }
 }
 
 export default App;
---
+++ src/Components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () => {
+    return(
+        <div>
+            <h3> This is my home section</h3>
+        </div>
+    )
+}
